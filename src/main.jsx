cafb1f56d2c5aa9f53ce7818b12b2dd64e8888db
pamu47 <pamudithamaduranga@gmail.com>
2019-04-10 17:52:01 +0530

    updated

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import AppContainer from './AppContainer';
-import NavComponent from './NavComponent';
-
-render(<AppContainer/>,document.getElementById('app'));
