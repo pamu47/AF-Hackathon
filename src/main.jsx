3ed09f1b553efcf403f390c839b8bfeb69a1cfa4
pamu47 <pamudithamaduranga@gmail.com>
2019-04-10 17:48:22 +0530

    Package bidding function and main

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AppContainer from './AppContainer';
+import NavComponent from './NavComponent';
+
+render(<AppContainer/>,document.getElementById('app'));
